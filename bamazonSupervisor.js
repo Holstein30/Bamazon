117f1228444224300b25e3a3abb39cd227c01a15
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2017-11-17 03:40:50 -0500

    supervisor finished

---
+++ bamazonSupervisor.js
@@ -0,0 +1,105 @@
+var mysql = require("mysql");
+var inquire = require("inquirer");
+
+var regex = /^\d+(?:\.\d{0,2})$/;
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    supervisorPrompt();
+});
+
+function supervisorPrompt() {
+    inquire.prompt([{
+        type: "list",
+        message: "Choose An Action",
+        choices: ["View Products Sales By Department", "Create New Department"],
+        name: "action"
+    }]).then(function (answers) {
+        chooseAction(answers.action);
+    });
+}
+
+function chooseAction(action) {
+    switch (action) {
+        case "View Products Sales By Department":
+            departmentSales();
+            break;
+        case "Create New Department":
+            newDepartment();
+            break;
+    }
+}
+
+function departmentSales() {
+    connection.query(
+        "SELECT departments.department_id, departments.department_name, departments.over_head_costs, " +
+        "product_sales, product_sales - over_head_costs AS total_profit FROM products " +
+        "RIGHT JOIN departments ON products.department_name = departments.department_name;",
+        function (err, result) {
+            if (err) throw err;
+            console.log("|| Department ID || Department Name || Over Head Costs || Product Sales || Total Profit ||");
+            for (var i in result) {
+                console.log("|| " + result[i].department_id + "             || " + result[i].department_name +
+                    "     || " + result[i].over_head_costs + "            || " + result[i].product_sales +
+                    "          || " + result[i].total_profit + "         ||");
+            }
+            setTimeout(finished, 5000);
+        }
+    )
+}
+
+function newDepartment() {
+    inquire.prompt([{
+        type: "input",
+        message: "What is the department name? ",
+        name: "department"
+    }, {
+        type: "input",
+        message: "What is the Over Head Cost? ",
+        name: "cost"
+    }]).then(function (answers) {
+        if (regex.test(answers.cost) && answers.department) {
+            console.log("Inserting new department...\n");
+            connection.query(
+                "INSERT INTO departments SET ?", {
+                    department_name: answers.department,
+                    over_head_costs: answers.cost
+                },
+                function (err, res) {
+                    console.log(res.affectedRows + " new department inserted!\n");
+                    setTimeout(finished, 2500);
+                }
+            );
+        } else {
+            console.log("\nEnsure you used the X.xx cost format & Department name isn't blank\n");
+            newDepartment();
+        }
+    });
+}
+
+function finished() {
+    inquire.prompt([{
+        type: "confirm",
+        message: "Is there anything else you would like to do? ",
+        name: "continue"
+    }]).then(function (answers) {
+        if (answers.continue) {
+            supervisorPrompt();
+        } else {
+            console.log("\nBamazon Supervisor signed out\n");
+            connection.end();
+        }
+    });
+}
--- schema.sql
+++ schema.sql
@@ -29,27 +29,60 @@ CREATE TABLE products
         (department_id)
 );
         INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
             ("Product1", "Department1", 1.99, 10);
         INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product2", "Department2", 2.99, 20);INSERT INTO products
+            ("Product2", "Department2", 2.99, 20);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product3", "Department3", 3.99, 30);INSERT INTO products
+            ("Product3", "Department3", 3.99, 30);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product4", "Department4", 4.99, 40);INSERT INTO products
+            ("Product4", "Department4", 4.99, 40);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product5", "Department5", 5.99, 50);INSERT INTO products
+            ("Product5", "Department5", 5.99, 50);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product6", "Department6", 6.99, 60);INSERT INTO products
+            ("Product6", "Department6", 6.99, 60);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product7", "Department7", 7.99, 70);INSERT INTO products
+            ("Product7", "Department7", 7.99, 70);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product8", "Department8", 8.99, 80);INSERT INTO products
+            ("Product8", "Department8", 8.99, 80);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
-            ("Product9", "Department9", 9.99, 90);INSERT INTO products
+            ("Product9", "Department9", 9.99, 90);
+        INSERT INTO products
+            (product_name, department_name, price, stock_quantity)
         VALUES
             ("Product10", "Department10", 10.99, 100);
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES("Department1", 1000.00);
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES("Department2", 2000.00);
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES("Department3", 3000.00);
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES("Department4", 4000.00);
+        INSERT INTO departments
+            (department_name, over_head_costs)
+        VALUES("Department5", 5000.00);
         SELECT *
         FROM products;
 
